85eee2da2aa2980421b6fd4050d0b54d1c4c7cc3
bitliner <giovanni.gaglione@gmail.com>
2015-03-15 02:43:59 +0100

    became node-static-2

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(require('express-bunyan-logger')({
-    name: 'logger',
-    streams: [{
-        level: 'info',
-        stream: process.stdout
-    }, {
-        level: 'info',
-        path: path.resolve(process.cwd(), 'logs/log'),
-        type: 'file'
-    }]
-}));
-app.use(express.static(path.join(__dirname, 'dist')));
+var cluster = require('express-cluster');
 
 
-var HTTP_PORT;
+cluster(function(worker) {
 
-HTTP_PORT = process.env.PORT || 9001;
 
-app.listen(HTTP_PORT, function() {
-    console.log('Listening on port ' + HTTP_PORT);
-});
+
+    var app = express();
+    var path = require('path');
+
+    app.use(require('express-bunyan-logger')({
+        name: 'logger',
+        streams: [{
+            level: 'info',
+            stream: process.stdout
+        }, {
+            level: 'info',
+            path: path.resolve(process.cwd(), 'logs/log'),
+            type: 'file'
+        }]
+    }));
+    app.use(express.static(path.join(__dirname, 'dist')));
+
+
+    var HTTP_PORT;
+
+    HTTP_PORT = process.env.PORT || 80;
+
+    app.listen(HTTP_PORT, function() {
+        console.log('Listening on port ' + HTTP_PORT);
+    });
+
+
+
+}, {});
+
+
